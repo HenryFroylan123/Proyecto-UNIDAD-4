14c19587f0be5a71c0a8ee2f8fde12a49133d6f9
Henry-Froylan-Chuc-Tec <56937480+HenryFroylan123@users.noreply.github.com>
2020-12-09 20:54:34 -0600

    Add files via upload
    print("Programa realizado por:\n"
          "-Henry Froylan Chuc Tec\n"
          "-Jose Natividad Tun Tun\n"
          "-Gelmon Hernandez Martinez")

---
+++ AnalizadorLexicoU4/archivo.dart
@@ -0,0 +1,11 @@
+void main() {
+	int a = 2;
+	int b = 3;
+	if (a>b){
+		print("hola");}
+	int smallNumber = a < b ? a : b;
+	print("$smallNumber is smaller");
+	String name = null;
+	String nameToPrint = name ?? "Guest User";
+	print(nameToPrint);
+}
